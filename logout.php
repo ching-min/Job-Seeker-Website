2cb99fd78a374d913d4968dd867cb5ff110ad817
ching-min <50642466+ching-min@users.noreply.github.com>
2020-02-20 00:06:10 -0800

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_save_path("/amd/cs/102/0216335/public_html");
-session_start(); 
-
-unset($_SESSION['username']);
-echo 'log out......';
-echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
-?>
